d845f8e34386497c84203c3373c23d9f8ab81aa2
fisal47 <yuliika707@gmail.com>
2021-11-08 15:49:16 +0700

    update v8

---
+++ message/mess.js
@@ -0,0 +1,17 @@
+module.exports.mess = {
+	        wait: 'PROSES...',
+			success: 'NI HASILNYA KAK',
+			wrongFormat: 'Format salah, coba liat lagi di menu',
+			error: {
+				api: 'APINYA HABIS BRO ISI ULANG DULU',
+				stick: 'ITU BUKAN STIKER KAK',
+				Iv: 'JANGAN NGASIH LINK GAJE TOD'
+			},
+			only: {
+				group: 'KHUSUS GRUP KAK',
+				admin: 'KHUSUS ADMIN GRUP KAK',
+				premium: 'KAMU BUKAN USER PREMIUM, SILAHKAN HUBUNGI OWNER',
+				owner: 'KHUSUS OWNER KAK',
+				Badmin: 'BOT HARUS JADI ADMIN KAK',
+			}
+		}
